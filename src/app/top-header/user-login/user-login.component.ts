fa066485e48cb3826fdafdab6768c8a4ef0dfd7e
sanimewu <2020-1-60-267@std.ewubd.edu>
2025-01-22 16:44:19 +0600

    #4 login ui

--- src/app/top-header/user-login/user-login.component.ts
+++ src/app/top-header/user-login/user-login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NzButtonComponent, NzButtonModule} from 'ng-zorro-antd/button';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzInputDirective} from 'ng-zorro-antd/input';
+import {NzDividerComponent} from 'ng-zorro-antd/divider';
 
 @Component({
   selector: 'app-user-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NzButtonComponent,
+    NzIconModule,
+    NzButtonModule,
+    NzInputDirective,
+    NzDividerComponent
+  ],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.scss'
 })
 export class UserLoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  submitLogin() {
+    console.log(this.loginForm.value);
+  }
 }
