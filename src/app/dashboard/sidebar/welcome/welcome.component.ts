43aa8fd7746a66cffdc91b6ee34ae202836e639f
sanimewu <2020-1-60-267@std.ewubd.edu>
2025-01-26 18:01:17 +0600

    #4 start

--- src/app/dashboard/sidebar/welcome/welcome.component.ts
+++ src/app/dashboard/sidebar/welcome/welcome.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [
+    NzButtonComponent
+  ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
